e884568a8231068fb1ce001e1f3de0167c5fed4e
LCMohale <lcmohale@gmail.com>
2021-08-20 10:06:14 +0200

    First Commit

---
+++ lib/apicalls/api.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+class AccSearchItemWidget extends StatelessWidget {
+  final AccSearchEntity _entity;
+
+  AccSearchItemWidget(this._entity);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        _entity.name,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      subtitle: SingleChildScrollView(
+        child: Text(_entity.accountBalance.toString()),
+      ),
+      onTap: () {},
+    );
+  }
+}
+
+class RequestService {
+  static Future<AccSearchResponse> query(String search) async {
+    var response = await http.get("https://us-central1-momentumtest-bfdef.cloudfunctions.net/app/api/v1/account/$search");
+    // Check if response is success
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      print('U made a request: for ${search} and result is ${response.body}');
+      var map = json.decode(response.body);
+      return AccSearchResponse.fromJson(map);
+    } else {
+      print("Query failed: ${search} ${response.body} (${response.statusCode})");
+      return null;
+    }
+  }
+}
+
+class AccSearchResponse {
+  String batchComplete;
+  AccQueryResponse query;
+  AccSearchResponse({this.batchComplete, this.query});
+
+  factory AccSearchResponse.fromJson(Map<String, dynamic> json) => AccSearchResponse(batchComplete: json["batchcomplete"], query: AccQueryResponse.fromJson(json["query"]));
+}
+
+class AccQueryResponse {
+  List<AccSearchEntity> search;
+
+  AccQueryResponse({this.search});
+
+  factory AccQueryResponse.fromJson(Map<String, dynamic> json) {
+    List<dynamic> resultList = json['search'];
+    List<AccSearchEntity> search = resultList.map((dynamic value) => AccSearchEntity.fromJson(value)).toList(growable: false);
+    return AccQueryResponse(search: search);
+  }
+}
+
+class AccSearchEntity {
+  int userId;
+  String name;
+  double accountNumber;
+  double accountBalance;
+
+  AccSearchEntity({this.userId, this.name, this.accountBalance, this.accountNumber});
+
+  factory AccSearchEntity.fromJson(Map<String, dynamic> json) => AccSearchEntity(userId: json["userId"], accountNumber: json["accountNumber"], name: json["name"], accountBalance: json["balance"]);
+}
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/cupertino.dart';
+import 'apicalls/api.dart';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Top Code User Api'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final searchTextController = TextEditingController();
+  List<AccSearchEntity> searchList = [];
+
+  void _search() {
+    String str = searchTextController.text;
+    RequestService.query(str).then((AccSearchResponse response) {
+      setState(() {
+        searchList = response.query.search;
+        if (searchList == null) {
+          searchList = [];
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Enter Account"),
+              Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Expanded(
+                  child: TextField(
+                    controller: searchTextController..text = "kf3awbHAIvk237HNec2p",
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Account Number',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 60,
+                  child: OutlineButton(
+                    onPressed: _search,
+                    highlightColor: Colors.lightBlue,
+                    child: Text("Login"),
+                  ),
+                ),
+              ]),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                child: OutlineButton(
+                  color: Colors.blueGrey,
+                  onPressed: () => {},
+                  highlightColor: Colors.lightBlue,
+                  child: Text("View Accounts"),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: ListView.builder(
+                    primary: false,
+                    itemBuilder: (BuildContext context, int index) => new AccSearchItemWidget(searchList[index]),
+                    itemCount: searchList.length,
+                    shrinkWrap: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
